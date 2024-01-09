66fdf7b600facfcc48755ccd7fc4f759f1450c75
meghamohan1414 <meghamohan1.414@gmail.com>
2024-01-09 11:28:43 -0800

    Changes to add and view products

--- product-service/src/main/java/com/shoppingservice/productservice/dto/ProductResponse.java
+++ product-service/src/main/java/com/shoppingservice/productservice/dto/ProductResponse.java
@@ -1,21 +1,18 @@
-package com.shoppingservice.productservice.model;
+package com.shoppingservice.productservice.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigDecimal;
 
-@Document(value = "product")
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
-public class Product {
-    @Id
+public class ProductResponse {
     private  String id;
     private String name;
     private String description;
--- product-service/src/test/java/com/shoppingservice/productservice/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/shoppingservice/productservice/ProductServiceApplicationTests.java
@@ -1,13 +1,61 @@
 package com.shoppingservice.productservice;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.shoppingservice.productservice.dto.ProductRequest;
+import net.bytebuddy.utility.dispatcher.JavaDispatcher;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.shaded.com.fasterxml.jackson.core.JsonProcessingException;
+import org.testcontainers.shaded.com.github.dockerjava.core.MediaType;
+
+import java.math.BigDecimal;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @SpringBootTest
+@Testcontainers
+@AutoConfigureMockMvc
 class ProductServiceApplicationTests {
 
+	@Container
+	static MongoDBContainer mongoDbContainer = new MongoDBContainer("mongo:4.4.2");
+	@Autowired
+	MockMvc mockMvc;
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	@DynamicPropertySource
+	static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
+		dynamicPropertyRegistry.add("spring.data.mongodb.uri", mongoDbContainer::getReplicaSetUrl);
+	}
+
 	@Test
-	void contextLoads() {
+	void createProductTest() throws Exception {
+		ProductRequest productRequest = getProductRequest();
+		String productRequestString=objectMapper.writeValueAsString(productRequest);
+		mockMvc.perform(MockMvcRequestBuilders.post("/api/product")
+				.contentType(MediaType.APPLICATION_JSON.getMediaType())
+				.content(productRequestString))
+				.andExpect(status().isCreated());
+	}
+
+	private ProductRequest getProductRequest() {
+		return ProductRequest.builder()
+				.name("iphone 15 pro max")
+				.description("iphone 15 pro max")
+				.price(BigDecimal.valueOf(1200))
+				.build();
 	}
 
 }
